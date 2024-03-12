fe59cd892b691eaacbf432c24fd7ea0facece7b2
sefogi <sebast18@gmail.com>
2024-03-12 22:25:05 +0100

    archivos y arquitectura base de frontend

---
+++ client/src/components/Layout.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+export const App =() =>{
+  
+
+  return (
+    <>
+      <h1>hola aiguas</h1>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/routes/Router.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../components/Layout"
+
+type Props = {}
+
+export const Route = (props: Props) => {
+  return (
+    <div>Route</div>
+  )
+}
+
+export default Route
